7bb1b18b811325f4a910f11ac2f3447e3a613cbd
gslay <mha220871@gmail.com>
2025-02-19 23:48:41 +0000

    update Warrior.java

--- Initial Code/source/Warrior.java
+++ Initial Code/source/Warrior.java
@@ -1,10 +1,14 @@
 public class Warrior extends Fighter {
     public Warrior(int baseHp, int wp) {
-        
+        super(baseHp, wp);
     }
 
     @Override
     public double getCombatScore() {
-        return 1.0;
+        if (Utility.isPrime(Battle.GROUND)) {
+            return getBaseHP()*2.0;
+        }
+        if (this.getWp() == 1) return getBaseHP();
+        return baseHP / 10.0;
     }
 }
