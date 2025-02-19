29211e41be830351f0923a224688b4e9410be4ad
gslay <mha220871@gmail.com>
2025-02-19 23:56:38 +0000

    update Utility.java ( add public static int getFibonacciIndex(int num) )

--- Initial Code/util/Utility.java
+++ Initial Code/util/Utility.java
@@ -42,9 +42,41 @@ public class Utility {
 	 */
 	public static boolean isFibonacci(int num) {
 		// Using fibonacci math formula.
-		if (num < 2) return false;
+		if (num <= 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
 		int test1 = 5*num*num + 4;
 		int test2 = 5*num*num - 4;
 		return isSquare(test1) || isSquare(test2);
 	}
+
+	public static int getFibonacciIndex(int num) {
+        if (num <= 0) {
+            return -1;
+        }
+        if (num == 1) {
+            return 1; // hoặc 2 tùy vào cách bạn định nghĩa chỉ số (F(1)=1 hoặc F(2)=1) - Ở đây chọn F(1)=1
+        }
+
+        int a = 1; // F(1)
+        int b = 1; // F(2)
+        int index = 2; // Bắt đầu từ chỉ số 2 (hoặc 3 nếu F(1) là chỉ số 1)
+
+        while (b < num) {
+            int nextFib = a + b;
+            a = b;
+            b = nextFib;
+            index++;
+
+            if (b == num) {
+                return index; // Tìm thấy số Fibonacci và trả về chỉ số
+            }
+        }
+
+        return -1; // Không tìm thấy số Fibonacci bằng num
+    }
+
 }
