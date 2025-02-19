66f8fb616ffb50ee2f44f2bede3ae951de535b96
gslay <mha220871@gmail.com>
2025-02-19 23:19:24 +0000

    update Utility.java

--- Initial Code/util/Utility.java
+++ Initial Code/util/Utility.java
@@ -11,7 +11,7 @@ public class Utility {
 		if (num < 2) {
 			return false;
 		}
-		for (int i = 2; i < num / 2; i++) {
+		for (int i = 2; i*i < num; i++) {
 			if (num % i == 0){
 				return false;
 			}
@@ -27,6 +27,12 @@ public class Utility {
 	 * @return <code>true</code> if <code>num</code> is a square number.
 	 */
 	public static boolean isSquare(int num) {
+		if (num < 0) {
+    		return false;
+		} // Số âm không phải số chính phương của số thực (chúng ta đang xét số nguyên dương)
+  		if (num == 0) {
+    		return true;
+		} // 0 là số chính phương (0*0 = 0)
 		int x = (int) Math.sqrt(num);
 		return x*x == num;
 	}
