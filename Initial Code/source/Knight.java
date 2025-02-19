7b08c030693a214c4f1211d42629cf8b825ba8d4
gslay <mha220871@gmail.com>
2025-02-19 23:29:32 +0000

    update Knight.java

--- Initial Code/source/Knight.java
+++ Initial Code/source/Knight.java
@@ -1,10 +1,12 @@
 public class Knight extends Fighter {
     public Knight(int baseHp, int wp) {
-        
+        super(baseHp, wp);
     }
 
     @Override
     public double getCombatScore() {
+        if (this.getWp() == 1) return getBaseHP();
+        else return baseHP / 10.0;
         return 1.0;
     }
 }
